3cc46c48bc3b28fe78e3a354b2ffa70444c89421
Stephanie Cheney <steph@scheneyphotography.com>
2020-05-20 17:41:54 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Song.destroy_all 
+
+Song.create(title: "lala", released: 0, release_year: 1999, artist_name: "Beyonce", genre: "Pop")
+Song.create(title: "laBoola", released: 1, release_year: 2001, artist_name: "Jay Z", genre: "Country")
+Song.create(title: "Honey", released: 1, release_year: 2005, artist_name: "Pharrell", genre: "Rap")
+Song.create(title: "Sugar", released: 0, release_year: 2010, artist_name: "Adele", genre: "R&B")
+
+byebug 
+0
