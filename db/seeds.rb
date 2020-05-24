7e7d896f3c68e9ea6ca39b11f70dbc9445952831
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-24 11:14:37 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Song.destroy_all
+
+Song.create(title: "T1", released: true, release_year: 2019, artist_name: "A1", genre: "G1")
+Song.create(title: "T2", released: true, release_year: 2015, artist_name: "A2", genre: "G2")
+Song.create(title: "T3", released: true, release_year: 2010, artist_name: "A1", genre: "G1")
+Song.create(title: "T4", released: true, release_year: 2015, artist_name: "A3", genre: "G2")
